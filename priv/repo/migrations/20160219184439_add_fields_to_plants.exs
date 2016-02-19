066bca3c0e9635d67ed75e0287700b97c2b4b597
Francois Ribemont <ribemont.francois@gmail.com>
2016-02-19 19:57:58 +0000

    Added new fields to plants. Hasn't been tested

--- priv/repo/migrations/20160219184439_add_fields_to_plants.exs
+++ priv/repo/migrations/20160219184439_add_fields_to_plants.exs
@@ -2,5 +2,11 @@ defmodule GrowJournal.Repo.Migrations.AddFieldsToPlants do
   use Ecto.Migration
 
   def change do
+    alter table(:plants) do
+      add :picture, :string
+      add :description, :string
+
+      timestamps
+    end
   end
 end
