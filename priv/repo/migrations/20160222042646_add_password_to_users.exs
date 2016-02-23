ddb6a4414201482dc92c37eb1d8cf1895b18fda2
Francois Ribemont <ribemont.francois@gmail.com>
2016-02-23 03:24:48 +0000

    Added the password to the user model

---
+++ priv/repo/migrations/20160222042646_add_password_to_users.exs
@@ -0,0 +1,9 @@
+defmodule GrowJournal.Repo.Migrations.AddPasswordToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :password_hash, :string
+    end
+  end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,11 +4,13 @@ defmodule GrowJournal.User do
   schema "users" do
     field :username, :string
     field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps
   end
 
-  @required_fields ~w(username)
+  @required_fields ~w(username password)
   @optional_fields ~w(email)
 
   @doc """
@@ -20,5 +22,22 @@ defmodule GrowJournal.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
